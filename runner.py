e90413b13323e020cb30cf2f77e91d9e6e44bf8f
Prak Sreypov <sreypovprak88@gmail.com>
2024-11-20 12:48:21 +0700

    add title to welcome page

--- runner.py
+++ runner.py
@@ -77,7 +77,7 @@ while True:
     if instructions:
 
         # Title
-        title = largeFont.render("Play Minesweeper", True, WHITE)
+        title = largeFont.render("Minesweeper Game", True, WHITE)
         titleRect = title.get_rect()
         titleRect.center = ((width / 2), 50)
         screen.blit(title, titleRect)
